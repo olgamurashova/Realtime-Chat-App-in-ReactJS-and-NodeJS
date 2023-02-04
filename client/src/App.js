0f97db8570b3dbcc545233f3c729d626395002b5
Olga Murashova <om.dreyfus@gmail.com>
2023-02-03 22:23:39 -0500

    extras

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 
 import io from "socket.io-client";
 
+import { useState } from "react";
+
 const socket = io.connect("http://localhost:3000");
 
 function App() {
 
+function App () {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
 
-  
+  //function to join room
+const joinRoom = [];
+};
+
   return (
     <div className='App'>
 
