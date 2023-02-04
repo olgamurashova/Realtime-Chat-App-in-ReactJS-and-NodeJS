a87717ca922c0d1c2ca24fc753587e0675af70b6
Olga Murashova <om.dreyfus@gmail.com>
2023-02-03 22:17:28 -0500

    "client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3000");
+
 function App() {
+
+
+  
   return (
     <div className='App'>
+
+      <h3>Join Chat</h3>
+      <input type="text" placeholder="Harry"/>
+      <input type="text" placeholder="Olga"/>
+      <button>Join a Room</button>
+      
 
     </div>
     
